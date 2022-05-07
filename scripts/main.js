123906bc05eea20ad0574beca85629ca3e19bbc9
JanssenSimon <32472583+JanssenSimon@users.noreply.github.com>
2022-05-07 12:54:16 -0400

    Adds toggling of previous round

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,17 @@
-/*TODO client side game logic*/
-const gamesection = document.getElementById("gamesection");
-
 let isButton = x => x.target.nodeName === "BUTTON";
 
+// Makes clicks visible
+const gamesection = document.getElementById("gamesection");
 gamesection.addEventListener("click", (event) => {
-  console.log("something got clicked");
   if (!isButton(event)) return;
-  console.log("Button clicked!");
   event.target.blur();
+})
+
+// Toggles the last round
+const lastroundrevealer = document.getElementById("lastroundrevealer");
+var lastroundcards = Array.from(document.getElementsByClassName("hidelastround"));
+lastroundrevealer.addEventListener("click", () => {
+    for (c of lastroundcards) {
+        c.classList.toggle("hidelastround");
+    }
 })
