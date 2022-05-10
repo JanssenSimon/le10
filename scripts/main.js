8a2c0a51d9fc838875a34a9e2e7fd9016812e7ef
JanssenSimon <32472583+JanssenSimon@users.noreply.github.com>
2022-05-10 00:03:02 -0400

    Adds quick plan for rest of my development effort

--- scripts/main.js
+++ scripts/main.js
@@ -22,6 +22,7 @@ let ws = new WebSocket("ws://localhost:5000");
 
 ws.onmessage = (message) => {
   console.log(message.data);
+  //update DOM
 }
 
 const playersCards = Array.from(document.getElementsByClassName("playerhand"));
