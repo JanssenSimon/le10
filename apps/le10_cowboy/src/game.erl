961b9ec9839401d602f3abf369fda28b9a94136f
JanssenSimon <32472583+JanssenSimon@users.noreply.github.com>
2023-12-15 22:18:40 -0500

    Adds plans for states of game

--- apps/le10_cowboy/src/game.erl
+++ apps/le10_cowboy/src/game.erl
@@ -3,3 +3,8 @@
 -behaviour(gen_statem).
 
 -export([]).
+
+% first state: wait for seating
+% second: betting
+% third through twelfth: tricks
+% last state: end of game (prompt for restart/quit game)
